d3fe07c887c435bb0ce0e85090a63e1742a8ae7b
Ilze Mitre <ilze.mitre@gmail.com>
2022-08-01 00:42:18 +0300

    fixed queries elements

--- js/main.js
+++ js/main.js
@@ -1,12 +1,14 @@
-document.querySelector('#push').onclick = function () {
-    if(document.querySelector('#newtask input').value.length == 0) {
+const newTaskInput = '#newtask input'
+
+document.getElementById('push').onclick = function () {
+    if(document.querySelector(newTaskInput).value.length == 0) {
         alert("Please Enter the Task!")
     } else {
         document.getElementById('tasks').innerHTML 
         += `
         <div class="task">
         <span id="taskname">
-            ${document.querySelector('#newtask input').value}
+            ${document.querySelector(newTaskInput).value}
         </span>
         </div>
         `;
